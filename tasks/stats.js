6e134ba688379fd132a7b5472386483b6d5c2529
t32k <ijok.ijok@gmail.com>
2013-02-18 23:31:23 +0900

    Initial Commit

---
+++ tasks/stats.js
@@ -0,0 +1,14 @@
+/*
+ * grunt-stats
+ * http://github.com/t32k/grunt-stats
+ * http://en.t32k.me
+ *
+ * Copyright (c) 2013 Koji Ishimoto
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+module.exports = function (grunt) {
+    
+};
