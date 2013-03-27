bc0c214ba6b99ef9d40cd9f64a7d667954a012c3
t32k <ijok.ijok@gmail.com>
2013-03-28 00:25:11 +0900

    Beautify code
    Signed-off-by: t32k <ijok.ijok@gmail.com>

--- tasks/phantom/report.js
+++ tasks/phantom/report.js
@@ -1,14 +1,16 @@
-var page	= require('webpage').create(),
-	system 	= require('system');
+var system = require('system'),
+	page = require('webpage').create();
 
-var g_path	= 'http://www.google-analytics.com/__utm.gif',
-	param	= '?utmwv=0&utmhn=en.t32k.me&utmt=event&utmcs=UTF-8&utmul=en-us&utmr=-&utmp=-&utmni=1',
-	utmac	= system.args[1],
-	utme	= '5(Final*'+ system.args[2] + '*' + system.args[3] + ')(' + system.args[4] + ')',
-	utmn 	= Math.floor(Math.random()*100000);
+var utm = 'http://www.google-analytics.com/__utm.gif',
+	etc = '?utmwv=0&utmhn=en.t32k.me&utmt=event&utmcs=UTF-8&utmul=en-us&utmr=-&utmp=-&utmni=1',
+	utmac = system.args[1],
+	utme = '5(iekara*' + system.args[2] + '*' + system.args[3] + ')(' + system.args[4] + ')',
+	utmn = Math.floor(Math.random() * 100000);
 
-var url 	= g_path + param +  '&utmac=' + utmac + '&utme=' + utme + '&utmn=' + utmn;
+var url = utm + etc + '&utmac=' + utmac + '&utme=' + utme + '&utmn=' + utmn;
 
 console.log(url);
 
-page.open(url, function () { phantom.exit(); });
+page.open(url, function () {
+	phantom.exit();
+});
--- tasks/stats.js
+++ tasks/stats.js
@@ -6,72 +6,66 @@
  * Copyright (c) 2013 Koji Ishimoto
  * Licensed under the MIT license.
  */
-
 'use strict';
 
 module.exports = function (grunt) {
 
-	var fs 		= require('fs'),
-		path 	= require('path'),
-		cp 		= require('child_process'),
-		moment 	= require('moment'),
+	var fs = require('fs'),
+		path = require('path'),
+		cp = require('child_process'),
+		moment = require('moment'),
 		phantom = require('phantomjs'),
 		binPath = phantom.path;
 
 	// Tasks
 	// ==========================================================================
-	grunt.registerMultiTask('stats', 'Statics of static files', function() {
+	grunt.registerMultiTask('stats', 'Statics of static files', function () {
 
-		var done = this.async();
+		var done = this.async(),
+			files = this.filesSrc,
+			opts = this.options({
+				ga_id: false,
+				total: false
+			});
 
-		var options = this.options({
-		  ga_id: false,
-		  total: false
-		});
-
-		if (!options.ga_id) {
+		if (!opts.ga_id) {
 			grunt.log.error('Google Analytics ID is REQUIRED.');
 			return false;
 		}
 
-		var files = this.filesSrc;
-
-		files.forEach(function(filepath) {
+		files.forEach(function (filepath) {
 
 			var stat = fs.statSync(filepath);
 			var time = moment(stat.mtime).format('"YYYY-MM-DD-HH:mm');
-			
-			grunt.log.writeln(filepath);
-			grunt.log.writeln(stat.size);
-			grunt.log.writeln(time);
 
-			postGA(options.ga_id, filepath, time, stat.size );
+			// grunt.log.writeln(filepath);
+			// grunt.log.writeln(stat.size);
+			// grunt.log.writeln(time);
+
+			postGA(opts.ga_id, filepath, time, stat.size);
 		});
 
 	});
 
 	// Helper
 	// ==========================================================================
-	
+
 	/* *
-     * Post data to Google Analytics
-     * @param {String} id
-     * @param {String} name
-     * @param {String} time
-     * @param {Number} size
-     */
-	var postGA = function( id, name, time, size ) {
-		grunt.log.writeln(binPath);
+	 * Post data to Google Analytics
+	 * @param {String} id
+	 * @param {String} name
+	 * @param {String} time
+	 * @param {Number} size
+	 */
+	var postGA = function (id, name, time, size) {
 
 		var childArgs = [
-		  path.join(__dirname, 'phantom/report.js'), id, name, time, size
+			path.join(__dirname, 'phantom/report.js'), id, name, time, size
 		];
-		grunt.log.writeln(childArgs);
-		cp.execFile(binPath, childArgs, function(err, stdout, stderr) {
-		  	grunt.log.error('err: '+err);
-		  	grunt.log.ok('stdout: '+stdout);
-		  	grunt.log.error('stderr: '+stderr);
+		cp.execFile(binPath, childArgs, function (err, stdout, stderr) {
+			grunt.log.error('err: ' + err);
+			grunt.log.writeln('stdout: ' + stdout);
+			grunt.log.writeln('stderr: ' + stderr);
 		});
-
 	};
 };
